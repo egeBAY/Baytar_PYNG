708f0dfd5d85bf415c1a52f47b9048374eb1ccaa
eren <115615808+ErenAtasun@users.noreply.github.com>
2023-05-04 13:26:43 +0300

    character dash
    dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
+    public float dashDistance = 5f; // The distance the character will dash
+    public float dashDuration = 0.5f; // The duration of the dash
+    public KeyCode dashKey = KeyCode.Space; // The key to press for dashing
+    
+    private bool isDashing = false; // Whether the character is currently dashing or not
+ 
     private Rigidbody2D rb;
 
     void Start()
@@ -20,5 +25,33 @@ public class PlayerMovement : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         Vector2 moveDir = new Vector2(x, y).normalized;
         rb.velocity = moveDir * moveSpeed;
+        // Check if the dash key is pressed and the character is not already dashing
+        if (Input.GetKeyDown(dashKey) && !isDashing)
+        {
+            StartCoroutine(Dash());
+        }   
+    }
+        private IEnumerator Dash()
+    {
+        isDashing = true;
+        
+        // Get the direction the character is facing
+        Vector2 direction = transform.right;
+        
+        // Calculate the destination position
+        Vector2 destination = (Vector2)transform.position + direction * dashDistance;
+        
+        // Calculate the duration of the dash
+        float duration = dashDuration * dashDistance / Vector2.Distance(transform.position, destination);
+        
+        // Move the character towards the destination position
+        while (Vector2.Distance(transform.position, destination) > 0.1f)
+        {
+            transform.position = Vector2.Lerp(transform.position, destination, Time.deltaTime / duration);
+            yield return null;
+        }
+        
+        isDashing = false;
     }
 }
+
