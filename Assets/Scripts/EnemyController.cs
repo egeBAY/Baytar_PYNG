44e8a398f51ffea97d0903a6174da2f0449b6965
eren <115615808+ErenAtasun@users.noreply.github.com>
2023-05-13 00:51:29 +0300

    EnemyMoveToPlayer

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,18 +7,21 @@ public class EnemyController : MonoBehaviour
     public float speed = 5f;
     public float maxHealth = 3f;
     public float health;
-
+    public float damage = 10f;
+    public float attackRange = 10f;
     
     private Transform player;
 
     private float distanceThreshold = 0.1f;
     private int currentIndex = 0;
     private GameObject waypoints;
-
+    
+    private GameObject baseObject;    
     private void Start()
     {
         health = maxHealth;
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        baseObject = GameObject.FindGameObjectWithTag("Base");
     }
 
     public void TakeDamage(float damageAmount)
@@ -38,6 +41,31 @@ public class EnemyController : MonoBehaviour
             direction.Normalize();
 
             transform.Translate(direction * speed * Time.deltaTime);
+            
         }
-    }    
+                // Calculate the distance between the enemy and the player
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        // Move towards the base if the object has the "Enemy" tag
+        if (gameObject.CompareTag("Enemy"))
+        {
+            Vector2 direction = (baseObject.transform.position - transform.position).normalized;
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+
+        // Attack the player if within range and the object has the "Player" tag
+        if (gameObject.CompareTag("Player") && distanceToPlayer <= attackRange)
+        {
+            AttackPlayer();
+        }
+    } 
+    private void AttackPlayer()
+    {
+        // Inflict damage to the player
+        PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }     
 }
